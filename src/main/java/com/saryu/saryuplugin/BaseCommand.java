b014e15bd34503376747d75ca00a0ec4951ca5aa
Sahur01-arch <rdxry450@gmail.com>
2026-01-31 19:13:23 +0700

    Update command logic and pom config

--- src/main/java/com/saryu/saryuplugin/BaseCommand.java
+++ src/main/java/com/saryu/saryuplugin/BaseCommand.java
@@ -1,63 +1,85 @@
 package com.saryu.saryuplugin;
 
-
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
-import org.jetbrains.annotations.NotNull;
-import net.milkbowl.vault.economy.EconomyResponse;
 
-int min = 1
-int max = 20
-
-int randomNum = (int)(Math.random() * ((max - min) + 1)) + min;
+import java.util.Random;
 
 public class BaseCommand implements CommandExecutor {
 
     private final Main plugin;
+    private final Random random = new Random();
 
     public BaseCommand(Main plugin) {
-        this.plugin = plugin; 
+        this.plugin = plugin;
     }
 
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        if (sender instanceof Player player) { 
+    public boolean onCommand(
+            @NotNull CommandSender sender,
+            @NotNull Command command,
+            @NotNull String label,
+            @NotNull String[] args
+    ) {
+
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage(Component.text(
+                    "Hanya Player yang bisa menggunakan command ini!",
+                    NamedTextColor.RED
+            ));
+            return true;
+        }
+
+        if (command.getName().equalsIgnoreCase("pvp")) {
             String worldName = plugin.getConfig().getString("pvp-world", "pvp");
 
-            player.sendMessage(Component.text("Teleporting to " + worldName + "...", NamedTextColor.GREEN));
-            
-            plugin.getServer().dispatchCommand(
-                plugin.getServer().getConsoleSender(), 
-                "mv tp " + player.getName() + " " + worldName
+            player.sendMessage(Component.text(
+                    "Teleporting ke " + worldName + "...",
+                    NamedTextColor.GREEN
+            ));
+
+            Bukkit.dispatchCommand(
+                    Bukkit.getConsoleSender(),
+                    "mv tp " + player.getName() + " " + worldName
             );
-        } else {
-            sender.sendMessage(Component.text("Hanya Player yang bisa menggunakan ini!!!", NamedTextColor.RED));
+            return true;
         }
-        return true;
-        
+
         if (command.getName().equalsIgnoreCase("gacha")) {
-          if (randomNum > 20) {
-            sender.sendMessage("§4You're unlucky");
-            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "eco give " + player.getName() + " 1")
-          }
-          else if (randomNum > 15) {
-            sender.sendMessage("§eKamu beruntung");
-            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "eco give " + player.getName() + " 5");
-          }
-          else if (randomNum > 10) {
-            sender.sendMessage("§4Wow");
-            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "eco give " + player.getName() + " 15")
-          }
-          else if (randomNum > 5) {
-            sender.sendMessage("§4You so lucky!")
-            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "eco give " + player.getName() + " 100")
-          }
+
+            int randomNum = random.nextInt(20) + 1; 
+            player.sendMessage("§7Gacha roll: §e" + randomNum);
+
+            if (randomNum > 15) {
+                player.sendMessage("§4You so lucky!");
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
+                        "eco give " + player.getName() + " 100");
+
+            } else if (randomNum > 10) {
+                player.sendMessage("§eWow!");
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
+                        "eco give " + player.getName() + " 15");
+
+            } else if (randomNum > 5) {
+                player.sendMessage("§aKamu beruntung");
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
+                        "eco give " + player.getName() + " 5");
+
+            } else {
+                player.sendMessage("§cYou're unlucky");
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
+                        "eco give " + player.getName() + " 1");
+            }
+
+            return true;
         }
-        }
+
+        return false;
     }
 }
-
